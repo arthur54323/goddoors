8eacd84ebbb3679f9007909c38299d2587d888d2
arthur54323 <130246135+arthur54323@users.noreply.github.com>
2023-04-13 19:46:19 -0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,5 +1,3 @@
- firesignal(game:GetService("ReplicatedStorage").EntityInfo.Caption.OnClientEvent, 'Godmode loaded, Have fun!',true,1)
-
 local LocalPlayer = game:GetService(“Players”).LocalPlayer
 local function Invincibility()
 if LocalPlayer.Character then
